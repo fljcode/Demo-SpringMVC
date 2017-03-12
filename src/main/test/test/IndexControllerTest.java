3baadd6b294d953be41914c5abfe7633f19e1de9
fanling5458@126.com <fanling5458@126.com>
2017-03-12 17:36:33 +0800

    注释

--- src/main/test/test/IndexControllerTest.java
+++ src/main/test/test/IndexControllerTest.java
@@ -1,20 +1,14 @@
 package test;
-import org.junit.Test;
+/*
 import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.InternalResourceView;
 
 import cn.jxufe.web.controller.IndexController;
 import junit.framework.TestCase;
-//引入静态方法standaloneSetup
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@ContextConfiguration (locations = {"classpath:applicationContext.xml" })
+@Configuration (locations = {"classpath:applicationContext.xml" })
 @RunWith (SpringJUnit4ClassRunner.class )
 public class IndexControllerTest extends TestCase {
 	
@@ -27,4 +21,4 @@ public class IndexControllerTest extends TestCase {
 		
 		mockMvc.perform(get("/index")).andExpect(view().name("home"));
 	}
-}
+}*/
